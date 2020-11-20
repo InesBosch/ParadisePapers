156e5ad36acc1128e258a1a024ec5ede9846996e
Ines Bosch-Alfonso <00monserrat@gmail.com>
2020-11-20 17:55:08 -0500

    template for queries

---
+++ queries.sql
@@ -0,0 +1,43 @@
+SET SEARCH_PATH TO paradise-papers, public;
+
+-- How many companies and individuals were involved per country and what is the percentage of the population that is corrupt?
+DROP TABLE IF EXISTS q1 cascade;
+
+CREATE TABLE q1 (
+);
+
+DROP VIEW X IF EXISTS CASCADE;
+
+create view X as 
+SELECT * FROM Country
+
+insert into q1
+
+
+-- How many countries were involved and what is the biggest tax haven?
+DROP TABLE IF EXISTS q2 cascade;
+
+CREATE TABLE q2 (
+);
+
+DROP VIEW X IF EXISTS CASCADE;
+
+create view X as 
+SELECT * FROM Country
+
+insert into q2
+
+
+
+-- Can we compare the human development index with the corruption perception index and reach a conclusion about how they are related?
+DROP TABLE IF EXISTS q3 cascade;
+
+CREATE TABLE q3 (
+);
+
+DROP VIEW X IF EXISTS CASCADE;
+
+create view X as 
+SELECT * FROM Country
+
+insert into q3
